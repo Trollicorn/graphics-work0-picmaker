eb106c2a56499f8ea001c0ac6ff9bbb1e0c38eaf
Mohammed Uddin <muddin3@stuy.edu>
2019-02-01 03:45:22 -0500

    random stuff to make some weird image

---
+++ artify.py
@@ -0,0 +1,12 @@
+import math
+
+pix = "P3 500 500 255\n"
+
+for r in range(500):
+    for c in range(500):
+        pix += str(r*c % 255) + " " + str(r * r * math.cos(c) % 255) + " " + str((r^2+c^3) % 255) + " "
+    pix += "\n"
+
+pic = open("art.ppm","w")
+pic.write(pix)
+pic.close()
