d74c81baa1af6a6a3c75ee62073183a2a73557fd
Mohammed Uddin <muddin3@stuy.edu>
2019-02-01 03:57:48 -0500

    fixed broken image problem

--- artify.py
+++ artify.py
@@ -4,7 +4,7 @@ pix = "P3 500 500 255\n"
 
 for r in range(500):
     for c in range(500):
-        pix += str(r*c % 255) + " " + str(r * r * math.cos(c) % 255) + " " + str((r^2+c^3) % 255) + " "
+        pix += str(r*c % 255) + " " + str(r * r * int(100*math.cos(c)) % 255) + " " + str((r^2+c^3) % 255) + " "
     pix += "\n"
 
 pic = open("art.ppm","w")
